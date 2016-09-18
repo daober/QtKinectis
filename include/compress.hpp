134bd35497f13ef8d9cee014ba195f33f612bace
Dan Obermaier <dan.obermaier@gmail.com>
2016-09-18 16:09:16 +0200

    defined compression

--- include/compress.hpp
+++ include/compress.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <pcl/compression/octree_pointcloud_compression.h>
+
 namespace processing{
+
     class compress{
+
         public:
+        compress();
+
+        int compressData(pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr outCloud);
 
         private:
-    
-    
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud_;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr outputCloud_;  
     };
 
 
--- src/compress.cpp
+++ src/compress.cpp
@@ -1,1 +1,24 @@
 #include "compress.hpp"
+
+#include <stdio.h>
+#include <sstream>
+#include <stdlib.h>
+#include <iostream>
+
+
+processing::compress::compress(){
+
+    //inputCloud_ = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    //outputCloud_ = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+}
+
+int processing::compress::compressData(pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr outCloud){
+
+    bool showStatistics = true;
+
+    std::stringstream compressedData;
+
+    //PointCloudEncoder->encodePointCloud(inCloud, outCloud);
+
+return (0);
+}
