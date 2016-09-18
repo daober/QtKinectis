a4a511b5ce05e3aea4dc9f34272c1b8d1dd0154e
Dan Obermaier <dan.obermaier@gmail.com>
2016-09-18 13:53:48 +0200

    added prototype filtering pointclouds

--- include/outlierremoval.hpp
+++ include/outlierremoval.hpp
@@ -1,13 +1,27 @@
 #pragma once
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+
 namespace processing{
 
     class outlierremoval{
     public:
 
+    outlierremoval();
+
+    int filter( pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud, 
+                pcl::PointCloud<pcl::PointXYZ>::Ptr outCloud);
+
+    pcl::PCDReader reader_;
+    pcl::PCDWriter writer_;
+
     private:    
-    
-    
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud_; 
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outputCloud_; 
+
     };
 
 
--- src/outlierremoval.cpp
+++ src/outlierremoval.cpp
@@ -1,1 +1,34 @@
 #include "outlierremoval.hpp"
+
+#include <iostream>
+#include <pcl/filters/statistical_outlier_removal.h>
+
+
+
+processing::outlierremoval::outlierremoval(){
+    
+    //allocate new cloud on heap via make shared
+    inputCloud_ = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    outputCloud_ = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();    
+}
+
+
+int processing::outlierremoval::filter(pcl::PointCloud<pcl::PointXYZ>::Ptr in, pcl::PointCloud<pcl::PointXYZ>::Ptr out){
+
+    std::cerr << "cloud outlierremoval called" <<std::endl;
+
+    reader_.read<pcl::PointXYZ>("filtered_scene_in.pcd", *in);
+
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+
+    sor.setInputCloud(in);
+
+    sor.setMeanK(50);
+    sor.setStddevMulThresh(1.0);
+    sor.filter(*out);
+
+    writer_.write<pcl::PointXYZ>("filtered_inliers.pcd", *out, false);
+
+    return(0);
+}
+
