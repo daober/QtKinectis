0e9ff4ea020640391977ef4dfbe43340760fc90d
Dan Obermaier <dan.obermaier@gmail.com>
2016-09-19 09:12:29 +0200

    added resampling method definition

--- include/resampling.hpp
+++ include/resampling.hpp
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/mls.h>
+
 
 namespace processing{
 
     class resampling{
+
     public:
 
+    int resampleXYZ(pcl::PointCloud<pcl::PointXYZ>::Ptr incloud, pcl::PointCloud<pcl::PointXYZ>::Ptr outcloud);
+    int resampleXYZRGB(pcl::PointCloud<pcl::PointXYZRGB>::Ptr incloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr outcloud);
+
     private:    
-    
+
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr treeXYZ_;
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr treeXYZRGB_;
+
     };
 
 }
--- src/resampling.cpp
+++ src/resampling.cpp
@@ -1,1 +1,49 @@
 #include "resampling.hpp"
+
+#include <boost/make_shared.hpp>
+
+
+int processing::resampling::resampleXYZ(pcl::PointCloud<pcl::PointXYZ>::Ptr in, pcl::PointCloud<pcl::PointXYZ>::Ptr out){
+
+    pcl::io::loadPCDFile("bun0.pcd", *in);
+
+    treeXYZ_ = boost::make_shared<pcl::search::KdTree<pcl::PointXYZ>>();
+    pcl::PointCloud<pcl::PointNormal> mls_points;
+
+    pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointNormal> mls;
+
+    mls.setComputeNormals(true);
+
+    mls.setInputCloud(in);
+    mls.setPolynomialFit(true);
+    mls.setSearchMethod(treeXYZ_);
+    mls.setSearchRadius(0.03);
+
+    mls.process(mls_points);
+
+    pcl::io::savePCDFile("bun0-mls.pcd", mls_points);
+
+}
+
+
+
+int processing::resampling::resampleXYZRGB(pcl::PointCloud<pcl::PointXYZRGB>::Ptr in, pcl::PointCloud<pcl::PointXYZRGB>::Ptr out){
+
+    pcl::io::loadPCDFile("bun0.pcd", *in);
+
+    treeXYZRGB_ = boost::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>();
+    pcl::PointCloud<pcl::PointNormal> mls_points;
+
+    pcl::MovingLeastSquares<pcl::PointXYZRGB, pcl::PointNormal> mls;
+
+    mls.setComputeNormals(true);
+
+    mls.setInputCloud(in);
+    mls.setPolynomialFit(true);
+    mls.setSearchMethod(treeXYZRGB_);
+    mls.setSearchRadius(0.03);
+
+    mls.process(mls_points);
+
+    pcl::io::savePCDFile("bun0-mls.pcd", mls_points);
+}
